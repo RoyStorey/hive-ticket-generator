9213b8f7216c437320334c34ef0254120f66b47e
RoyStorey <imlejiii@gmail.com>
2023-05-12 17:43:42 -0400

    front-end makeover.

--- app.py
+++ app.py
@@ -13,76 +13,82 @@ from dash.dependencies import Input, Output, State
 app = Dash(__name__, external_stylesheets=['/assets/global.css'])
 
 app.layout = html.Div(className='content-container', children=[
-    html.Div(className='form-container', children=[
-        html.Div(className='form-gap', children=[
-            html.Label('Operator Initials:'),
-            dcc.Input(id='initials-input'),
-        ]),
-        html.Div(className='form-gap', children=[
-            html.Label('IDs:'),
-            dcc.Textarea(id='id-input'),
-        ]),
-        html.Div(className='form-gap', children=[
-            html.Label('MITRE ATT&CK Vectors:'),
-            dcc.Textarea(id='attack-vector-input'),
-        ]),
-        html.Div(className='form-gap', children=[
-            html.Label('Suricata Alerts:'),
-            dcc.Textarea(id='alert-input'),
-        ]),
-        html.Div(className='form-gap', children=[
-            html.Label('Description:'),
-            dcc.Textarea(id='description-input'),
-        ]),
-        html.Div(className='form-gap', children=[
-            html.Label('Recommended Solution:'),
-            dcc.Textarea(id='solution-input'),
-        ]),
-    ]),
-    html.Div([
-        dcc.Upload(
-            id='pcap-upload',
-            children=html.Div([
-                'Drag and Drop .CSV or ',
-                html.A('Select .CSV file')
+    html.Div(children=[
+        html.Div(className='form-container', children=[
+            html.Div(className='form-gap', children=[
+                html.Label('Operator Initials:'),
+                dcc.Input(id='initials-input'),
             ]),
-            style={
-                'width': '100%',
-                'height': '60px',
-                'lineHeight': '60px',
-                'borderWidth': '1px',
-                'borderStyle': 'dashed',
-                'borderRadius': '5px',
-                'textAlign': 'center',
-                # 'margin': '10px'
-            },
-            # Allow multiple files to be uploaded
-        ),
-        html.Div(id='output-data-upload')
-    ]),
-    html.Div([
-        dcc.Upload(
-            id='observable-upload',
-            children=html.Div([
-                'Drag and Drop Observables or ',
-                html.A('Select Observables files')
+            html.Div(className='form-gap', children=[
+                html.Label('IDs:'),
+                dcc.Textarea(id='id-input'),
             ]),
-            style={
-                # 'width': '100%',
-                'height': '60px',
-                'lineHeight': '60px',
-                'borderWidth': '1px',
-                'borderStyle': 'dashed',
-                'borderRadius': '5px',
-                'textAlign': 'center',
-                'margin-top': '10px'
-            },
-            # Allow multiple files to be uploaded
-            multiple=True
-        ),
+            html.Div(className='form-gap', children=[
+                html.Label('MITRE ATT&CK Vectors:'),
+                dcc.Textarea(id='attack-vector-input'),
+            ]),
+            html.Div(className='form-gap', children=[
+                html.Label('Suricata Alerts:'),
+                dcc.Textarea(id='alert-input'),
+            ]),
+            html.Div(className='form-gap', children=[
+                html.Label('Description:'),
+                dcc.Textarea(id='description-input'),
+            ]),
+            html.Div(className='form-gap', children=[
+                html.Label('Recommended Solution:'),
+                dcc.Textarea(id='solution-input'),
+            ]),
+        ]),
+        html.Div([
+            dcc.Upload(
+                id='pcap-upload',
+                children=html.Div([
+                    'Drag and Drop .CSV or ',
+                    html.A('Select .CSV file')
+                ]),
+                className='upload-div',
+                style={
+                    'width': '100%',
+                    'height': '60px',
+                    'lineHeight': '60px',
+                    'borderWidth': '1px',
+                    'borderStyle': 'dashed',
+                    # 'borderRadius': '5px',
+                    'textAlign': 'center',
+                    # 'margin': '10px'
+                },
+                # Allow multiple files to be uploaded
+            ),
+            html.Div(id='output-data-upload')
+        ]),
+        html.Div([
+            dcc.Upload(
+                id='observable-upload',
+                children=html.Div([
+                    'Drag and Drop Observables or ',
+                    html.A('Select Observables files')
+                ]),
+                className='upload-div',
+                style={
+                    # 'width': '100%',
+                    'height': '60px',
+                    'lineHeight': '60px',
+                    'borderWidth': '1px',
+                    'borderStyle': 'dashed',
+                    # 'borderRadius': '5px',
+                    'textAlign': 'center',
+                    'margin-top': '10px'
+                },
+                # Allow multiple files to be uploaded
+                multiple=True
+            ),
+        ]),
+        html.Button('Format!', id='submit-button', n_clicks=0),
     ]),
-    html.Button('Submit', id='submit-button', n_clicks=0),
-    html.Div(id='formatted_output')
+    html.Div( className='output-container',children=[
+        html.Div(id='formatted_output')
+    ]),
 ])
 
 
@@ -164,4 +170,4 @@ for row in dataFromCSV:
 # print("**Time Observed:** ", formattedHiveCase.timeObserved, "by " + formattedHiveCase.initials +  "\n\n**Src IP:** " + formattedHiveCase.sourceIP + "\n**Src Ports:** " + formattedHiveCase.sourcePorts + "\n\n**Dst IP:** " + formattedHiveCase.destinationIP + "\n**Dst Ports:** " + formattedHiveCase.destinationPorts + "\n\n**Community IDs:**\n" + formattedHiveCase.communityIds + "\n\n**IDs:**\n" + formattedHiveCase.ids + "\n\n**Observables/Hashes:**\n" + formattedHiveCase.observableHashes + "\n\n**MITRE Vectors of Attack** " + formattedHiveCase.mitreVectors + "\n\n**Suricata Alerts:** " + formattedHiveCase.suricataAlerts + "\n\n**Description:** " + formattedHiveCase.description + "\n\n**Recommended Solution:** " + formattedHiveCase.recommendedSolution)
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server(debug=False)

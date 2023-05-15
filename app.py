867f52593ed45404784804e30e2c90dcbbe701e5
Brandon <bfree98@icloud.com>
2023-05-14 20:28:16 -0400

    added file hashing, csv parsing, output of rough final product

--- app.py
+++ app.py
@@ -1,16 +1,30 @@
 from datetime import datetime
 import csv
-import glob
 import hashlib
-import base64
-import datetime
-import io
-import pandas as pd
-from dash import Dash, html, dcc, callback, Output, Input, dash_table
-from dash.dependencies import Input, Output, State
+import os
+import glob
+from dash import Dash, html, dcc, Output, Input
+import dash_uploader as du
+from dash.dependencies import Input, Output
+csv_data = {
+    'start_list': [],
+    'stop_list': [],
+    'src_ip_list': [],
+    'src_country_list': [],
+    'src_port_list': [],
+    'dst_ip_list': [],
+    'dst_country_list': [],
+    'dst_port_list': [],
+    'uri_list': [],
+    'community_id_list': []
+}
+hashes = {}
+UPLOAD_FOLDER_ROOT = 'upload/'
 
+app = Dash(__name__, external_stylesheets=[
+           '/assets/global.css'], prevent_initial_callbacks=True)
 
-app = Dash(__name__, external_stylesheets=['/assets/global.css'])
+du.configure_upload(app, UPLOAD_FOLDER_ROOT, use_upload_id='')
 
 app.layout = html.Div(className='content-container', children=[
     html.Div(children=[
@@ -18,10 +32,6 @@ app.layout = html.Div(className='content-container', children=[
             html.Div(className='form-gap', children=[
                 html.Label('Operator Initials:'),
                 dcc.Input(id='initials-input'),
-            ]),
-            html.Div(className='form-gap', children=[
-                html.Label('IDs:'),
-                dcc.Textarea(id='id-input'),
             ]),
             html.Div(className='form-gap', children=[
                 html.Label('MITRE ATT&CK Vectors:'),
@@ -41,63 +51,76 @@ app.layout = html.Div(className='content-container', children=[
             ]),
         ]),
         html.Div([
-            dcc.Upload(
-                id='pcap-upload',
-                children=html.Div([
-                    'Drag and Drop .CSV or ',
-                    html.A('Select .CSV file')
-                ]),
-                className='upload-div',
-                style={
-                    'width': '100%',
-                    'height': '60px',
-                    'lineHeight': '60px',
-                    'borderWidth': '1px',
-                    'borderStyle': 'dashed',
-                    # 'borderRadius': '5px',
-                    'textAlign': 'center',
-                    # 'margin': '10px'
-                },
-                # Allow multiple files to be uploaded
+            du.Upload(
+                text='Click here or Drag a .csv file to upload',
+                id='csv-upload',
+                upload_id=None,
+                filetypes=['csv'],
             ),
-            html.Div(id='output-data-upload')
         ]),
         html.Div([
-            dcc.Upload(
+            du.Upload(
+                text='Click or Drag an Observable to upload',
+                upload_id=None,
+                max_files=10,
                 id='observable-upload',
-                children=html.Div([
-                    'Drag and Drop Observables or ',
-                    html.A('Select Observables files')
-                ]),
-                className='upload-div',
-                style={
-                    # 'width': '100%',
-                    'height': '60px',
-                    'lineHeight': '60px',
-                    'borderWidth': '1px',
-                    'borderStyle': 'dashed',
-                    # 'borderRadius': '5px',
-                    'textAlign': 'center',
-                    'margin-top': '10px'
-                },
-                # Allow multiple files to be uploaded
-                multiple=True
             ),
         ]),
         html.Button('Format!', id='submit-button', n_clicks=0),
     ]),
-    html.Div( className='output-container',children=[
-        html.Div(id='formatted_output')
+    html.Div(className='output-container', children=[
+        html.Div(id='formatted-output'),
+        html.Div(id='callback-output-1'),
+        html.Div(id='callback-output-2')
     ]),
 ])
 
 
+@du.callback(
+    output=Output('callback-output-1', 'children'),
+    id='csv-upload'
+)
+def parse_csv(filenames):
+    filename = glob.glob('upload/*.csv')
+    with open(filename[0], 'r') as file:
+        reader = csv.reader(file)
+        next(reader)
+        for row in reader:
+            start_time, stop_time, src_ip, src_country, src_port, dst_ip, dst_country, dst_port, uri, community_id = row
+            csv_data['start_list'].append(start_time)
+            csv_data['stop_list'].append(stop_time)
+            csv_data['src_ip_list'].append(src_ip)
+            csv_data['src_country_list'].append(src_country)
+            csv_data['src_port_list'].append(src_port)
+            csv_data['dst_ip_list'].append(dst_ip)
+            csv_data['dst_country_list'].append(dst_country)
+            csv_data['dst_port_list'].append(dst_port)
+            csv_data['uri_list'].append(uri)
+            csv_data['community_id_list'].append(community_id)
+        file.close()
+        os.remove(filename[0])
+    #         li = html.Li(src_ip)
+    #         stop_list.append(li)
+    # return html.Ul(start_list)
+
+
+@du.callback(
+    output=Output('callback-output-2', 'children'),
+    id='observable-upload'
+)
+def hash_observables(status: du.UploadStatus):
+    files = os.listdir('upload/')
+    for file in files:
+        with open(file, 'rb') as f:
+            data = f.read()
+            sha256 = hashlib.sha256(data).hexdigest()
+            hashes[file] = sha256
+        os.remove(os.path.join('upload', file))
+
+
 @app.callback(
-    Output('formatted_output', 'children'),
-    Input('pcap-upload', 'contents'),
-    Input('observable-upload', 'contents'),
+    Output('formatted-output', 'children'),
     Input('initials-input', 'value'),
-    Input('id-input', 'value'),
     Input('attack-vector-input', 'value'),
     Input('alert-input', 'value'),
     Input('description-input', 'value'),
@@ -105,42 +128,44 @@ app.layout = html.Div(className='content-container', children=[
     Input('submit-button', 'n_clicks')
 
 )
-def update_output(pcap_contents, observable_contents, initials, ids, attack_vector, alerts, description, solution, n_clicks):
+def update_output(initials, attack_vector, alerts, description, solution, n_clicks):
     if n_clicks > 0:
-        return html.Div(f'pcap = {pcap_contents}, observable={observable_contents}, initials={initials}, id={ids}, vector={attack_vector}, alert={alerts}, description={description}, solution={solution}')
+        # print(csv_data)
+        # print(hashes)
+        return dcc.Textarea(value=f'initials={initials}, vector={attack_vector}, alert={alerts}, description={description}, solution={solution},{str(hashes)}, {str(csv_data)}')
 
 
-filenames = glob.glob("./case-files/*")
-filehashes = []
+# filenames = glob.glob("./case-files/*")
+# filehashes = []
 
-i = 1
-for filename in filenames:
-    with open(filename, 'rb') as inputfile:
-        data = inputfile.read()
-        with open('hashedfiles', 'a') as file:
-            file.write(hashlib.md5(data).hexdigest() + '\n')
-            filehashes.append(' ' + str(i) + '. ' +
-                              hashlib.md5(data).hexdigest())
-            i = i+1
+# i = 1
+# for filename in filenames:
+#     with open(filename, 'rb') as inputfile:
+#         data = inputfile.read()
+#         with open('hashedfiles', 'a') as file:
+#             file.write(hashlib.md5(data).hexdigest() + '\n')
+#             filehashes.append(' ' + str(i) + '. ' +
+#                               hashlib.md5(data).hexdigest())
+#             i = i+1
 
 
-dataFile = open(r"sessions.csv")
-dataFromCSV = csv.reader(dataFile, delimiter=',', skipinitialspace=True)
-j = 1
-SrcIP, DstIP, SrcPorts, DstPorts, CommIDs = [], [], [], [], []
-next(dataFromCSV)
-for row in dataFromCSV:
-    if ' ' + row[2] not in SrcIP:
-        SrcIP.append(' ' + row[2])
-    if ' ' + row[4] not in SrcPorts:
-        SrcPorts.append(' ' + row[4])
-    if ' ' + row[5] not in DstIP:
-        DstIP.append(' ' + row[5])
-    if ' ' + row[7] not in DstPorts:
-        DstPorts.append(' ' + row[7])
-    if str(j) + '. ' + row[9] not in CommIDs:
-        CommIDs.append(str(j) + '. ' + row[9])
-        j = j+1
+# dataFile = open(r"sessions.csv")
+# dataFromCSV = csv.reader(dataFile, delimiter=',', skipinitialspace=True)
+# j = 1
+# SrcIP, DstIP, SrcPorts, DstPorts, CommIDs = [], [], [], [], []
+# next(dataFromCSV)
+# for row in dataFromCSV:
+#     if ' ' + row[2] not in SrcIP:
+#         SrcIP.append(' ' + row[2])
+#     if ' ' + row[4] not in SrcPorts:
+#         SrcPorts.append(' ' + row[4])
+#     if ' ' + row[5] not in DstIP:
+#         DstIP.append(' ' + row[5])
+#     if ' ' + row[7] not in DstPorts:
+#         DstPorts.append(' ' + row[7])
+#     if str(j) + '. ' + row[9] not in CommIDs:
+#         CommIDs.append(str(j) + '. ' + row[9])
+#         j = j+1
 
 
 # now = datetime.now()
@@ -170,4 +195,4 @@ for row in dataFromCSV:
 # print("**Time Observed:** ", formattedHiveCase.timeObserved, "by " + formattedHiveCase.initials +  "\n\n**Src IP:** " + formattedHiveCase.sourceIP + "\n**Src Ports:** " + formattedHiveCase.sourcePorts + "\n\n**Dst IP:** " + formattedHiveCase.destinationIP + "\n**Dst Ports:** " + formattedHiveCase.destinationPorts + "\n\n**Community IDs:**\n" + formattedHiveCase.communityIds + "\n\n**IDs:**\n" + formattedHiveCase.ids + "\n\n**Observables/Hashes:**\n" + formattedHiveCase.observableHashes + "\n\n**MITRE Vectors of Attack** " + formattedHiveCase.mitreVectors + "\n\n**Suricata Alerts:** " + formattedHiveCase.suricataAlerts + "\n\n**Description:** " + formattedHiveCase.description + "\n\n**Recommended Solution:** " + formattedHiveCase.recommendedSolution)
 
 if __name__ == '__main__':
-    app.run_server(debug=False)
+    app.run_server(debug=True)
